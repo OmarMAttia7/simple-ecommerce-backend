73532d7cf0fd2671d72f4f2423b98318da060318
hazel97 <omarmattia97@gmail.com>
2022-12-19 21:49:29 +0200

    feat: add db connection functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        username: configService.get('db.username'),
+        password: configService.get('db.password'),
+        database: configService.get('db.database'),
+        host: configService.get('db.host'),
+        port: +configService.get('db.port')
+      }),
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      }
+    }),
     UsersModule,
     ConfigModule.forRoot({
-      load: [configuration],
+      load: [config],
     }),
   ],
 })
